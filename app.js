6bfb0f5230b16a756844eab3431a7b7559e44473
Phonsing <129599660+Polsing@users.noreply.github.com>
2023-12-23 03:53:16 +0700

    23/12/2023

--- app.js
+++ app.js
@@ -3,14 +3,33 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const User = require("./data/data.json");
 const  app = express();
-const PORT = process.env.PORT;
+const productRouter = express.Router();
+const PORT = 5000;
 
 app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname,"public/")));
+app.use(express.static(path.join(__dirname,"./public/")));
+app.set("views","./src/views");
+app.set("view engine","ejs");
 
-app.get("/",(req,res)=>{
-    res.send("APL-shop");
+productRouter.route("/").get((req,res)=>{
+    res.render("products",{User,});
+});
+
+productRouter.route("/:id").get((req,res)=>{
+    const id = req.params.id;
+    res.render("product",{
+        product: User[id],
+    })
+})
+
+app.use("/product",productRouter)
+
+
+app.get("/Login",(req,res)=>{
+    res.render("index",{email: '@email',pass: 'password'});
+   
 })
 
 app.listen(PORT, ()=>{
